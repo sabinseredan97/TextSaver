c5038888c418261127d94d115fee67ae42c1bfe9
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-10-11 14:16:36 +0300

    update

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-export async function GET() {
-  return NextResponse.status(202).json({ message: "Salutari" });
+export async function GET(req, res) {
+  return res.status(202).json({ message: "Salutari" });
 }
