f23a14b9098bcd9e37ea41c7b42f67c57d8b9bb2
sabinseredan97 <114243765+sabinseredan97@users.noreply.github.com>
2023-10-11 14:09:43 +0300

    update

--- src/app/api/hello/route.js
+++ src/app/api/hello/route.js
@@ -3,11 +3,5 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET() {
-  const session = await getServerSession(authOptions);
-
-  if (!session) {
-    return NextResponse.json({ message: "You are not logged in." });
-  }
-
-  return NextResponse.json({ name: session.user.name });
+  return NextResponse.json({ message: "Salutari" });
 }
